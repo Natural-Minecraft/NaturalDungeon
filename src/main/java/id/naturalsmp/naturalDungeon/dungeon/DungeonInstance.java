020013e3e1120f7f57e992b9dc237adb275149f2
Natural-Minecraft <naturalsmp.mc@gmail.com>
2026-02-21 11:29:06 +0700

    Fix build error: Restored BarStyle import in DungeonInstance

--- src/main/java/id/naturalsmp/naturalDungeon/dungeon/DungeonInstance.java
+++ src/main/java/id/naturalsmp/naturalDungeon/dungeon/DungeonInstance.java
@@ -10,6 +10,7 @@ import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
